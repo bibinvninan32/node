9c128d0b98475d2e9fc6bbc92fc4419bfcb95b1d
EC2 Default User <ec2-user@ip-10-0-1-202.eu-west-2.compute.internal>
2024-01-20 12:52:17 +0000

    init

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var application = express();
-application.get('/', function (req, res) {
+var app = express();
+app.get('/', function (req, res) {
   res.send('Hello World.. from nodejs!');
 });
-application.listen(3000, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
